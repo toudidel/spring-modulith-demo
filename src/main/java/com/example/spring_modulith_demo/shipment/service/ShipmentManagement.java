6d5460ec95246594398590b12cc99000d5f26121
k.kozlowski <k.kozlowski@reply.com>
2025-07-31 16:20:21 +0200

    Time machine

--- src/main/java/com/example/spring_modulith_demo/shipment/service/ShipmentManagement.java
+++ src/main/java/com/example/spring_modulith_demo/shipment/service/ShipmentManagement.java
@@ -1,9 +1,12 @@
 package com.example.spring_modulith_demo.shipment.service;
 
 import com.example.spring_modulith_demo.order.OrderCreatedEvent;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.modulith.events.ApplicationModuleListener;
+import org.springframework.modulith.moments.HourHasPassed;
+import org.springframework.modulith.moments.support.TimeMachine;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,10 +15,18 @@ import org.springframework.stereotype.Component;
 public class ShipmentManagement {
 
   private final ShipmentService shipmentService;
+  private final TimeMachine timeMachine;
 
   @ApplicationModuleListener
   public void onOrderCreated(OrderCreatedEvent event) throws InterruptedException {
     Thread.sleep(5000);
     shipmentService.send(event.orderId());
+    log.info("Current time is " + timeMachine.now().toString());
+    timeMachine.shiftBy(Duration.ofHours(3));
+  }
+
+  @ApplicationModuleListener
+  public void onHourHasPassed(HourHasPassed event) throws InterruptedException {
+    log.info("Hour has passed. Now is: " + event.getTime().toString());
   }
 }

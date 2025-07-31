4ddc80d2f6c426c2f0e4d6a81df9050eb9b34e9d
k.kozlowski <k.kozlowski@reply.com>
2025-07-31 13:15:06 +0200

    Initial commit

---
+++ src/main/java/com/example/spring_modulith_demo/SpringModulithDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring_modulith_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModulithDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringModulithDemoApplication.class, args);
+  }
+}
